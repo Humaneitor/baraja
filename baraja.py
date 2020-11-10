9ad0347ea729c865109938382bfc89b95a93205c
Ismael Bonilla <ibonilla@DESKTOP-KE9GC80.BAOPARTN.COM>
2020-11-10 23:30:28 +0100

    Funcion creaBaraja funcionando

--- baraja.py
+++ baraja.py
@@ -2,7 +2,9 @@ palos = ['o','c','e','b']
 numeros = ['A','2','3','4','5','6','7','S','C','R']
 baraja = []
 
-for palo in palos:
-    for numero in numeros:
-        baraja.append(numero+palo)
-print (baraja)
+def creaBaraja()
+    for palo in palos:
+        for numero in numeros:
+            baraja.append(numero+palo)
+    return baraja
+
