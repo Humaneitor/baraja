e7648bd9aaaacdbbaf1bdb33abc4923fee3f7694
Ismael Bonilla <ibonilla@DESKTOP-KE9GC80.BAOPARTN.COM>
2020-11-11 23:00:15 +0100

    baraja barajeada

--- baraja.py
+++ baraja.py
@@ -1,3 +1,4 @@
+import random
 palos = ['o','c','e','b']
 numeros = ['A','2','3','4','5','6','7','S','C','R']
 
@@ -7,3 +8,16 @@ def creaBaraja():
         for numero in numeros:
             baraja.append(numero+palo)
     return baraja
+
+
+def barajar(lista):
+    for i in range(len(lista)):
+        nueva_pos = random.randrange(len(lista))
+        aux = lista[nueva_pos]
+        lista[nueva_pos]=lista[i]
+        lista[i] = aux
+    return lista
+
+print(barajar(creaBaraja()))
+
+
